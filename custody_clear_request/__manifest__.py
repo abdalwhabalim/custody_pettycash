8739b2c75c70d45fa214249b0ae9526b6b515ecc
Abdelwhab Babikr <abdalwhab.babeker@yahoo.com>
2023-03-15 00:08:17 +0400

    Add files via upload

---
+++ custody_clear_request/__manifest__.py
@@ -0,0 +1,37 @@
+{
+    'name': 'Custody Clear Request',
+    'version': '1.0',
+    'summary': """
+    This module allows you to request some amount and reconcile with specific expenses account or return as a cash, with dynamic 
+    approval process for (Cash request/Cash Reconcile)
+    """,
+    'category': 'Accounting',
+    'author': 'SGT',
+    'support': 'support@softguidetech.com',
+    'website': 'https://softguidetech.com',
+    'license': 'OPL-1',
+    'currency': 'EUR',
+    'data': [
+
+        'security/security_view.xml',
+        'security/ir.model.access.csv',
+        'views/custody_clear_request_view.xml',
+        'views/custody_approval_route.xml',
+        'views/res_config_settings_views.xml',
+        'data/custody_approval_route.xml',
+        'reports/custody_report.xml',
+    ],
+    'depends': ['base','account','analytic','custody_request'],
+
+
+
+
+    'installable': True,
+    'application': True,
+
+
+
+
+
+
+}
