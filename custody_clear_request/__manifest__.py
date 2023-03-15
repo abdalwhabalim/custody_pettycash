241c282f6703c7db065391b934c549c1ea69e002
Abdelwhab Babikr <abdalwhab.babeker@yahoo.com>
2023-03-15 07:59:57 +0400

    Update __manifest__.py

--- custody_clear_request/__manifest__.py
+++ custody_clear_request/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'Custody Clear Request / Petty cash',
+    'name': 'Petty cash / Cahs Reconcile',
     'version': '1.0',
     'summary': """
     This module allows you to request some amount and reconcile with specific expenses account or return as a cash, with dynamic 
